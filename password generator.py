a2f95230c72b16d71c9c097fda940fa610ac1044
Cheik-Siramakan Keita <cheiksiramakankeita@gmail.com>
2019-12-06 01:23:33 +0100

    ...

--- password generator.py
+++
@@ -1,7 +0,0 @@
-import random
-chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789/\|!@#$%?&*()_+-=][}{^;.,`~°><:\"'"
-password = ""
-
-for c in range(20):
-    password += random.choice(chars)
-print(password)
--- username scrambler.py
+++
@@ -1,7 +0,0 @@
-import random
-
-x = "Keita"
-l  = list(x)
-random.shuffle(l)
-y = ''.join(l)
-print(y)
